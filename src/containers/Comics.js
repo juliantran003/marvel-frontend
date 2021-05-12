8db9c15cf5d5a2d837610a56bfa8ee3ab6be2de9
Julian Tran <juliantran@julians-macbook-air.home>
2021-05-12 13:44:27 +0200

    comics linked with axios

--- src/containers/Comics.js
+++ src/containers/Comics.js
@@ -1,5 +1,41 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Comics = () => {
-  return <div>Comics</div>;
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://marvel-backend-julian.herokuapp.com/comics"
+        );
+
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <span>Loading...</span>
+  ) : (
+    <div>
+      <h1>Comics</h1>
+      {data.results.map((results, index) => {
+        return (
+          <div>
+            <img src={results.thumbnail.path} alt="" />
+            <p>{results.title}</p>
+            <p>{results.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Comics;
